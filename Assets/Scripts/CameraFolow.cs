f5e66eeda101c2cce5e508b45d23e9a8604fd80a
SkeryGame <131359747+SkeryGame@users.noreply.github.com>
2024-11-08 17:57:14 +0100

    greier

--- Assets/Scripts/CameraFolow.cs
+++ Assets/Scripts/CameraFolow.cs
@@ -6,6 +6,6 @@ public class CameraFolow : MonoBehaviour
 
     private void LateUpdate()
     {
-        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
     }
 }
